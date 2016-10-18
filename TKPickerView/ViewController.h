5cbe5054f09b3291b7928c6a47eb37826bef4a25
“Tushar <tushar.kolhe01@gmail.com>
2016-10-18 14:32:29 +0530

    UIPickerView Implemented

--- TKPickerView/ViewController.h
+++ TKPickerView/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIPickerViewDelegate, UIPickerViewDelegate>
 
+
+   
+
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
--- TKPickerView/ViewController.m
+++ TKPickerView/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+{
+    NSArray *planets;
+}
 @end
 
 @implementation ViewController
@@ -17,11 +20,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+        self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"galaxy.jpg"]];
+    planets=@[@"Sun",@"Mercury",@"Vinus",@"Earth",@"Mars",@"Jupiter",@"Saturn",@"Uranus",@"Neptune"];
+   
+        self.picker.dataSource=self;
+        self.picker.delegate=self;
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+}
+
+- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+// The number of rows of data
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [planets count];
+}
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return planets[row];
 }
 
 @end
