e9047f863a061ed0b2e281a41b757a7c4c142065
Felix ITs 01 <felixits01@felixs-macbook-pro-2.local>
2016-10-18 12:39:23 +0530

    Initial Commit

---
+++ TKPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TKPickerView
+//
+//  Created by Felix ITs 01 on 18/10/16.
+//  Copyright © 2016 Tushar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TKPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TKPickerView
+//
+//  Created by Felix ITs 01 on 18/10/16.
+//  Copyright © 2016 Tushar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
