f2ebdc5069db5ddee49bfd376971ab110c9ebbe1
“Tushar <tushar.kolhe01@gmail.com>
2016-10-23 11:29:40 +0530

    Show  selected row from picker view on labels

--- TKPickerView/ViewController.h
+++ TKPickerView/ViewController.h
@@ -10,10 +10,22 @@
 
 @interface ViewController : UIViewController<UIPickerViewDelegate, UIPickerViewDataSource>
 
+{
+    NSArray *config;
+    NSMutableDictionary *configurationDictionary;
+    NSArray *companies;
+    NSArray *processors;
+    NSArray *rams;
+    NSArray *harddisks;
+}
 - (IBAction)doneButton:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
 
 @property (weak, nonatomic) IBOutlet UILabel *companyLbl;
    
+@property (weak, nonatomic) IBOutlet UILabel *processorLbl;
+@property (weak, nonatomic) IBOutlet UILabel *ramLbl;
+@property (weak, nonatomic) IBOutlet UILabel *hardDiskLbl;
 
 @property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
--- TKPickerView/ViewController.m
+++ TKPickerView/ViewController.m
@@ -10,9 +10,6 @@
 
 @interface ViewController ()
 
-{
-    NSArray *config;
-}
 @end
 
 @implementation ViewController
@@ -23,16 +20,23 @@
     
         self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"picimg.jpg"]];
     
-    config=@[@[@"Apple",  @"Dell", @"Acer", @"Lenevo", @"hp"],
-             @[@"dual core",  @"i3", @"i5", @"i7", @"64-bit Xeon"],
-             @[@"1gb",  @"2gb", @"4gb", @"8gb", @"16gb"],
-             @[@"128",  @"256Gb", @"512GB", @"1TB", @"121 Flash"]];
+    companies = @[@"Apple",  @"Dell", @"Acer", @"Lenevo", @"hp"];
+    rams = @[@"1gb",  @"2gb", @"4gb", @"8gb", @"16gb"];
+    harddisks = @[@"128",  @"256Gb", @"512GB", @"1TB", @"121 Flash"];
+    processors = @[@"dual core",  @"i3", @"i5", @"i7", @"64-bit Xeon"];
+    config=@[companies,
+             processors,
+             rams,
+             harddisks
+             ];
     
     
     
     NSLog(@"%@",config);
         self.picker.dataSource=self;
         self.picker.delegate=self;
+    
+    configurationDictionary = [[NSMutableDictionary alloc]init];
     
 }
 
@@ -44,7 +48,7 @@
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
-    return 4;
+      return config.count;
 }
 
 // The number of rows of data
@@ -52,7 +56,23 @@
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    return 5;
+    if (component == 0) {
+        return companies.count;
+    }
+    else if (component == 1) {
+        return processors.count;
+    }
+    else if (component == 2) {
+        return rams.count;
+    }
+    else if (component == 3) {
+        return harddisks.count;
+    }
+    else {
+        return 0;
+    }
+
+
 }
 
 // The data to return for the row and component (column) that's being passed in
@@ -61,12 +81,56 @@
 
 - (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
-    return config[component][row];
+    return [[config objectAtIndex:component] objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    
+    NSArray *temp = [config objectAtIndex:component];
+    
+    NSString *value = [temp objectAtIndex:row];
+    
+    NSLog(@"%@",value);
+    
+    if (component == 0) {
+        //value shows company name
+        
+        [configurationDictionary setValue:value forKey:@"company"];
+    }
+    else if (component == 1) {
+        //value shows processor name
+        [configurationDictionary setValue:value forKey:@"processor"];
+
+    }
+    else if (component == 2) {
+        //value shows ram name
+        [configurationDictionary setValue:value forKey:@"ram"];
+
+    }
+    else if (component == 3) {
+        //value shows harddisk name
+        [configurationDictionary setValue:value forKey:@"hard_disk"];
+
+    }
+    else {
+        NSLog(@"%@",configurationDictionary);
+    }
+    NSLog(@"%@",configurationDictionary);
+
+
 }
 
 - (IBAction)doneButton:(id)sender {
-    
-    
+   
+    NSLog(@"%@",configurationDictionary);
+    _labelTitle.text=@"Your Configuration is:";
+    if (configurationDictionary) {
+        self.companyLbl.text = [configurationDictionary valueForKey:@"company"];
+        self.processorLbl.text = [configurationDictionary valueForKey:@"processor"];
+        self.ramLbl.text= [configurationDictionary valueForKey:@"ram"];
+        self.hardDiskLbl.text = [configurationDictionary valueForKey:@"hard_disk"];
+    }
 }
 
 
